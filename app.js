db6a8d0306e97978480b146a9a131ab30374eeac
balogun14 <awwalbalogun06@gmail.com>
2023-08-29 02:03:42 +0100

    used selectors in the dom

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 //using selectors inside the element
-// traversing the dom
-
-
-
-
-// this is traversing the dom option
-const btns = document.querySelectorAll(".question-btn");
-
-btns.forEach(function (btn) {
+const questions = document.querySelectorAll(".question");
+questions.forEach(function (question) {
+  // console.log(question);'
+  btn = question.querySelector(".question-btn");
+  // console.log(btn);
   btn.addEventListener("click", (e) => {
-    var parent = e.currentTarget.parentElement.parentElement;
-    parent.classList.toggle('show-text')
+    questions.forEach(function (item) {
+      if (item != question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
   });
 });
+
+// this is traversing the dom option
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", (e) => {
+//     var parent = e.currentTarget.parentElement.parentElement;
+//     parent.classList.toggle('show-text')
+//   });
+// });
